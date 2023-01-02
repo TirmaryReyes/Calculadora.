cc3e8435358f423ca9b9a787d0ac74c15d522580
TirmaryReyes <119057900+TirmaryReyes@users.noreply.github.com>
2023-01-02 11:39:53 +0100

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,41 @@
+'use strict';
+
+function operation() {
+  let number1 = prompt('enter the first number');
+  let sign = prompt('enter +,-,/,* sign');
+  let number2 = prompt('enter the second number');
+  let result = [];
+
+  if (isNaN(number1) || isNaN(number2)) {
+    console.log("⛔️You can't do this operation!");
+  }
+  if (number1 && !number2) {
+    return `the square root ${number1} is: ${Number(
+      Math.sqrt(number1).toFixed(3)
+    )}`;
+  } else if (number2 && !number1) {
+    return `the square root ${number2} is: ${Number(
+      Math.sqrt(number2).toFixed(3)
+    )}`;
+  } else if (sign == '+') {
+    return `The result of your operation is: ${(result = parseFloat(
+      Number(number1 + number2).toFixed(3)
+    ))}`;
+  } else if (sign == '-') {
+    return `The result of your operation is: ${(result = parseFloat(
+      Number(number1 - number2).toFixed(3)
+    ))}`;
+  } else if (sign == '*') {
+    return `The result of your operation is: ${(result = parseFloat(
+      Number(number1 * number2).toFixed(3)
+    ))}`;
+  } else if (sign == '/') {
+    return `The result of your operation is: ${(result = parseFloat(
+      Number(number1 / number2).toFixed(3)
+    ))}`;
+  } else {
+    return 'Bye.';
+  }
+}
+const test = operation();
+console.log(test);
