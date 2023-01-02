2303588b9b2454c53eff7d29cb07296e33dd138a
TirmaryReyes <119057900+TirmaryReyes@users.noreply.github.com>
2023-01-02 14:10:08 +0100

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,39 +1,41 @@
 'use strict';
 
-function operation() {
-    let number1 = parseFloat(prompt('enter the first number'));
+  
+  function operation() {
+    let number1 = Number(prompt('enter the first number'));
     let sign = prompt('enter +,-,/,* sign');
-    let number2 = parseFloat(prompt('enter the second number'));
+    let number2 = Number(prompt('enter the second number'));
     let result = [];
   
     if (isNaN(number1) || isNaN(number2)) {
-      console.log("⛔️You can't do this operation!");
-    }
-    if (number1 && !number2) {
-      console.log(`the square root ${number1} is: ${Number(Math.sqrt(number1).toFixed(
+      result="⛔️You can't do this operation!";
+
+    }else if(number1&&!number2){
+    result=`the square root ${number1} is: ${Number(Math.sqrt(number1).toFixed(
         3
-      ))}`);
+      ))}`;
+  
     } else if (number2 && !number1) {
-      console.log(`the square root ${number2} is: ${Number(Math.sqrt(number2).toFixed(
-        3
-      ))}`);
+        result=`the square root ${number2} is: ${Number(Math.sqrt(number2).toFixed(
+          3
+        ))}`;
     } else if (sign == '+') {
-      result = Number((number1+ number2).toFixed(3));   
+      result =`The result of your operation is: ${parseFloat(Number((number1+ number2).toFixed(3)))}`;   
     } else if (sign == '-') {
-      result = Number((number1 - number2).toFixed(3));
+      result = `The result of your operation is: ${parseFloat(Number((number1 - number2).toFixed(3)))}`;
      
     } else if (sign == '*') {
-      result = Number((number1 * number2).toFixed(3));
+      result =`The result of your operation is:  ${parseFloat(Number((number1 * number2).toFixed(3)))}`;
       
     } else if (sign == '/') {
-     result = Number((number1 / number2).toFixed(3));
+     result = `The result of your operation is:  ${parseFloat(Number((number1 / number2).toFixed(3)))}`;
       
-    } else {
-      return 'Bye.';
+    } else{
+      return 'Bye';
     }
   
-    return `The result of your operation is: ${result}`;
+    return `${result}`;
   }
   const test = operation();
-  console.log(test);
+  console.log(test)
   
