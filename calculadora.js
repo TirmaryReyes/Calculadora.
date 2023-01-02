900e28b36ffac268f2a4455a9a61b5a06526ca21
TirmaryReyes <119057900+TirmaryReyes@users.noreply.github.com>
2023-01-02 13:09:57 +0100

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,40 +1,39 @@
 'use strict';
 
 function operation() {
-  let number1 = prompt('enter the first number');
-  let sign = prompt('enter +,-,/,* sign');
-  let number2 = prompt('enter the second number');
-  let result = [];
-
-  if (isNaN(number1) || isNaN(number2)) {
-    console.log("⛔️You can't do this operation!");
+    let number1 = parseFloat(prompt('enter the first number'));
+    let sign = prompt('enter +,-,/,* sign');
+    let number2 = parseFloat(prompt('enter the second number'));
+    let result = [];
+  
+    if (isNaN(number1) || isNaN(number2)) {
+      console.log("⛔️You can't do this operation!");
+    }
+    if (number1 && !number2) {
+      console.log(`the square root ${number1} is: ${Number(Math.sqrt(number1).toFixed(
+        3
+      ))}`);
+    } else if (number2 && !number1) {
+      console.log(`the square root ${number2} is: ${Number(Math.sqrt(number2).toFixed(
+        3
+      ))}`);
+    } else if (sign == '+') {
+      result = Number((number1+ number2).toFixed(3));   
+    } else if (sign == '-') {
+      result = Number((number1 - number2).toFixed(3));
+     
+    } else if (sign == '*') {
+      result = Number((number1 * number2).toFixed(3));
+      
+    } else if (sign == '/') {
+     result = Number((number1 / number2).toFixed(3));
+      
+    } else {
+      return 'Bye.';
+    }
+  
+    return `The result of your operation is: ${result}`;
   }
-  if (number1 && !number2) {
-    return `the square root ${number1} is: ${Number(
-      Math.sqrt(number1).toFixed(3)
-    )}`;
-  } else if (number2 && !number1) {
-    return `the square root ${number2} is: ${Number(
-      Math.sqrt(number2).toFixed(3)
-    )}`;
-  } else if (sign == '+') {
-    return `The result of your operation is: ${Number((result = parseFloat(Number(number1) + Number(number2)).toFixed(3)
-    ))}`;
-  } else if (sign == '-') {
-    return `The result of your operation is: ${(result = parseFloat(
-      Number(number1 - number2).toFixed(3)
-    ))}`;
-  } else if (sign == '*') {
-    return `The result of your operation is: ${(result = parseFloat(
-      Number(number1 * number2).toFixed(3)
-    ))}`;
-  } else if (sign == '/') {
-    return `The result of your operation is: ${(result = parseFloat(
-      Number(number1 / number2).toFixed(3)
-    ))}`;
-  } else {
-    return 'Bye.';
-  }
-}
-const test = operation();
-console.log(test);
+  const test = operation();
+  console.log(test);
+  
