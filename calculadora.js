bbb355583de656d60356ff2e6e78daa6f89896bd
TirmaryReyes <119057900+TirmaryReyes@users.noreply.github.com>
2023-01-03 12:37:32 +0100

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -2,40 +2,43 @@
 
   
   function operation() {
-    let number1 = Number(prompt('enter the first number'));
-    let sign = prompt('enter +,-,/,* sign');
-    let number2 = Number(prompt('enter the second number'));
-    let result = [];
-  
-    if (isNaN(number1) || isNaN(number2)) {
-      result="⛔️You can't do this operation!";
+  let number1 = Number(prompt('enter the first number'));
 
-    }else if(number1&&!number2){
-    result=`the square root ${number1} is: ${Number(Math.sqrt(number1).toFixed(
-        3
-      ))}`;
-  
-    } else if (number2 && !number1) {
-        result=`the square root ${number2} is: ${Number(Math.sqrt(number2).toFixed(
-          3
-        ))}`;
-    } else if (sign == '+') {
-      result =`The result of your operation is: ${parseFloat(Number((number1+ number2).toFixed(3)))}`;   
-    } else if (sign == '-') {
-      result = `The result of your operation is: ${parseFloat(Number((number1 - number2).toFixed(3)))}`;
-     
-    } else if (sign == '*') {
-      result =`The result of your operation is:  ${parseFloat(Number((number1 * number2).toFixed(3)))}`;
-      
-    } else if (sign == '/') {
-     result = `The result of your operation is:  ${parseFloat(Number((number1 / number2).toFixed(3)))}`;
-      
-    } else{
-      return 'Bye';
-    }
-  
-    return `${result}`;
+  let number2 = Number(prompt('enter the second number'));
+    
+  let sum = parseFloat((number1 + number2).toFixed(3));
+  let subtr = parseFloat((number1 - number2).toFixed(3));
+  let div = parseFloat((number1 / number2).toFixed(3));
+  let mult = parseFloat((number1 * number2).toFixed(3));
+  let result = [];
+
+  if (isNaN(number1) || isNaN(number2)) {
+    return "⛔️You can't do this operation!";
+  } else if (number1 && !number2) {
+    return `the square root ${number1} is: ${Number(
+      Math.sqrt(number1).toFixed(3)
+    )}`;
+  } else if (number2 && !number1) {
+    return `the square root ${number2} is: ${Number(
+      Math.sqrt(number2).toFixed(3)
+    )}`;
+  } else if (parseFloat(sum)) {
+    sum = `${Number(sum.toFixed(3))}`;
+  } else if (parseFloat(subtr)) {
+    subtr = `${Number(subtr.toFixed(3))}`;
+  } else if (parseFloat(div)) {
+    div = `${Number(div.toFixed(3))}`;
+  } else if (parseFloat(mult)) {
+    mult = `${Number(mult.toFixed(3))}`;
+  } else {
+    return 'Bye';
   }
-  const test = operation();
-  console.log(test)
-  
+  result.push(`The result of your sum is: ${sum}`);
+  result.push(`The result of your subtration is: ${subtr}`);
+  result.push(`The result of your division is: ${div}`);
+  result.push(`The result of your multiplication is: ${mult}`);
+  return `${result}`;
+}
+const test = operation();
+console.log(test);
+
